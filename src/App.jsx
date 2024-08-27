fdc34ac9a8bc02299dc198d835f30209b7c4973f
Allison <allisonabinger@gmail.com>
2024-08-27 13:02:46 -0500

    A - 08/27 01:02 PM - Added extra styling to About Me section, still needs responsive styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,57 @@ import Header from "./components/Header";
 import Section from "./components/Section";
 import HelpfulResource from "./components/HelpfulResource";
 import AboutMe from "./components/AboutMe";
+import Footer from "./components/Footer";
 
 function App() {
-    return (
-        <div>
-          <Header />
-          <Section title="What is react?">
-            React is a JavaScript library developed by Meta (formerly Facebook) used to make powerful interactive user interfaces.
-            It uses a component-based architecture for a modular and reusable build that can be easier to scale and maintain. React also improves performance
-            by utilizing a virtual DOM to efficiently update changes in the UI and render it to the user. With a strong ecosystem that uses other tools and libraries and Redux,
-            Next.js, and React Router, it provides a strong and flexible framework for dynmaic web applications and interfaces.
-          </Section>
-          <Section title="Benefits of React">
-            <ul>
-                <li>Modular - Easily reuse components within your application.</li>
-                <li>Active Community - React remains a popular framework with an active community</li>
-                <li>Cross Platform - React works within iOS and Android apps</li>
-                <li>Efficient Performance - Using the Virtual DOM allows for efficient user interactions and data chagnes</li>
-                <li>Custom Dev Tools - Official React Developer Tools extensions are available for Chrome and Firefox</li>
-            </ul>
-          </Section>
-          <Section title="Helpful Resources">
-            <HelpfulResource label="W3 Schools Full React Tutorial" link="https://www.w3schools.com/REACT/default.asp"/>
-            <HelpfulResource label="BroCode on Youtube - React Tutorial Playlist" link="https://www.youtube.com/playlist?list=PLZPZq0r_RZOMQArzyI32mVndGBZ3D99XQ"/>
-            <HelpfulResource label="Creating a React Project with Vite" link="https://vitejs.dev/guide/"/>
-          </Section>
-          <AboutMe />
-        </div>
-      );
+  return (
+    <div>
+      <Header />
+      <Section title="What is react?">
+        React is a JavaScript library developed by Meta (formerly Facebook) used
+        to make powerful interactive user interfaces. It uses a component-based
+        architecture for a modular and reusable build that can be easier to
+        scale and maintain. React also improves performance by utilizing a
+        virtual DOM to efficiently update changes in the UI and render it to the
+        user. With a strong ecosystem that uses other tools and libraries and
+        Redux, Next.js, and React Router, it provides a strong and flexible
+        framework for dynmaic web applications and interfaces.
+      </Section>
+      <Section title="Benefits of React">
+        <ul>
+          <li>Modular - Easily reuse components within your application.</li>
+          <li>
+            Active Community - React remains a popular framework with an active
+            community
+          </li>
+          <li>Cross Platform - React works within iOS and Android apps</li>
+          <li>
+            Efficient Performance - Using the Virtual DOM allows for efficient
+            user interactions and data chagnes
+          </li>
+          <li>
+            Custom Dev Tools - Official React Developer Tools extensions are
+            available for Chrome and Firefox
+          </li>
+        </ul>
+      </Section>
+      <Section title="Helpful Resources">
+        <HelpfulResource
+          label="W3 Schools Full React Tutorial"
+          link="https://www.w3schools.com/REACT/default.asp"
+        />
+        <HelpfulResource
+          label="BroCode on Youtube - React Tutorial Playlist"
+          link="https://www.youtube.com/playlist?list=PLZPZq0r_RZOMQArzyI32mVndGBZ3D99XQ"
+        />
+        <HelpfulResource
+          label="Creating a React Project with Vite"
+          link="https://vitejs.dev/guide/"
+        />
+      </Section>
+      <AboutMe />
+    </div>
+  );
 }
 
 export default App;
--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,18 +4,21 @@ function AboutMe() {
   return (
     <div className="about-me">
       <img src={portrait} alt="Photo of Allison Binger"></img>
-      <p>
-        Howdy! I'm Allison Binger, a software engineer and full stack web
-        developer. I'm currently finishing my education at Atlas School Tulsa
-        and will be graduating in December 2024, but I have certifications in
-        Cybersecurity Analytics and Network Administration. I love web design
-        and front-end development, but I thoroughly enjoy data analytics and ETL
-        data projects. I have two corgis, Gouda and Mozzarella, and a corn snake
-        named Noodle. I love travelling and camping, although I have a phobia of
-        insects! Once I graduate, I hope to enter the tech industry under a
-        supportive and collaborative team to find my calling within the field
-        and gather experience as an engineer!
-      </p>
+      <div className="about-me-text">
+        <h3>About Me</h3>
+        <p>
+            Howdy! I'm Allison Binger, a software engineer and full stack web
+            developer. I'm currently finishing my education at Atlas School Tulsa
+            and will be graduating in December 2024, but I have certifications in
+            Cybersecurity Analytics and Network Administration. I love web design
+            and front-end development, but I thoroughly enjoy data analytics and ETL
+            data projects. I have two corgis, Gouda and Mozzarella, and a corn snake
+            named Noodle. I love travelling and camping, although I have a phobia of
+            insects! Once I graduate, I hope to enter the tech industry under a
+            supportive and collaborative team to find my calling within the field
+            and gather experience as an engineer!
+        </p>
+      </div>
     </div>
   );
 }
